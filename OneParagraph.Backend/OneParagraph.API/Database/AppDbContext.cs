0cd204db83bd0b6f4576324ce3cdb02f5c737794
olivermiracle <adam.cisz@pm.me>
2024-09-28 18:07:44 +0200

    Add aspnet identity

--- OneParagraph.Backend/OneParagraph.API/Database/AppDbContext.cs
+++ OneParagraph.Backend/OneParagraph.API/Database/AppDbContext.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using OneParagraph.Shared.Domain.Identity;
 
 namespace OneParagraph.API.Database;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext<IdentityUser>
 {
-    public DbSet<User> Users { get; set; }
+    public AppDbContext(DbContextOptions options) : base(options) { }
 }
--- OneParagraph.Backend/OneParagraph.Shared/Domain/Identity/User.cs
+++
@@ -1,9 +0,0 @@
-namespace OneParagraph.Shared.Domain.Identity;
-
-public class User
-{
-    public string Email { get; set; }
-    public string AccountKey { get; set; }
-    public string VerificationKey { get; set; }
-    
-}
