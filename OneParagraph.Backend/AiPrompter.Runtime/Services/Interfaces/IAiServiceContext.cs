5499248499ae1c42b526d154ad22e7fac2118d9b
m653l <sebastian.kura@pm.me>
2024-09-29 05:08:31 +0200

    Some stuff

--- OneParagraph.Backend/AiPrompter.Runtime/Services/Interfaces/IAiServiceContext.cs
+++ OneParagraph.Backend/AiPrompter.Runtime/Services/Interfaces/IAiServiceContext.cs
@@ -1,4 +1,5 @@
 ﻿using AiPrompter.Runtime.Models;
+using OneParagraph.Shared.Content;
 using OneParagraph.Shared.Enums;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -8,4 +9,5 @@ namespace AiPrompter.Runtime.Services.Interfaces;
 public interface IAiServiceContext
 {
     Task<List<(Industries, string)>> PromptAi(Dictionary<Industries, List<MarketauxGetNewsByCategoryResponse>> newsFromApi);
+    Task<List<(Stock, string)>> PromptAiFormStock(Dictionary<Stock, List<MarketauxGetNewsByCategoryResponse>> newsFromApi);
 }
---
+++ OneParagraph.Backend/OneParagraph.API/Endpoints/CreateStock.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using OneParagraph.API.Database;
+using OneParagraph.Shared.Content;
+
+namespace OneParagraph.API.Endpoints;
+
+public sealed class CreateStock : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("api/create-stock", async (RequestW request, AppDbContext context) =>
+        {
+            var stock = new Stock()
+            {
+                Id = Guid.NewGuid(),
+                Symbol = request.Symbol,
+                Name = request.Symbol   // From Yahoo Finance API
+            };
+
+            context.Stocks.Add(stock);
+
+            await context.SaveChangesAsync();
+
+            return Results.Ok();
+        })
+        .RequireAuthorization();
+    }
+
+    internal class RequestW
+    {
+        public string Symbol { get; set; }
+    }
+}
