92d2d3e7fd92f760d185174f260cc7fce17b0841
m653l <sebastian.kura@pm.me>
2024-09-29 04:00:26 +0200

    Adds stock request

--- OneParagraph.Backend/AiPrompter.Runtime/Services/Interfaces/INewsDataPollerService.cs
+++ OneParagraph.Backend/AiPrompter.Runtime/Services/Interfaces/INewsDataPollerService.cs
@@ -1,4 +1,5 @@
 ﻿using AiPrompter.Runtime.Models;
+using OneParagraph.Shared.Content;
 using OneParagraph.Shared.Enums;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -8,4 +9,5 @@ namespace AiPrompter.Runtime.Services.Interfaces;
 public interface INewsDataPollerService
 {
     Task<Dictionary<Industries, List<MarketauxGetNewsByCategoryResponse>>> GetCategoryNewsAsync();
+    Task<Dictionary<Stock, List<MarketauxGetNewsByCategoryResponse>>> GetStockNewsAsync();
 }
--- OneParagraph.Backend/AiPrompter.Runtime/Services/NewsDataPollerService.cs
+++ OneParagraph.Backend/AiPrompter.Runtime/Services/NewsDataPollerService.cs
@@ -1,5 +1,8 @@
-﻿using AiPrompter.Runtime.Models;
+﻿using AiPrompter.Runtime.Database;
+using AiPrompter.Runtime.Models;
 using AiPrompter.Runtime.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using OneParagraph.Shared.Content;
 using OneParagraph.Shared.Enums;
 using OneParagraph.Shared.Extensions;
 using RestSharp;
@@ -11,7 +14,7 @@ using System.Threading.Tasks;
 
 namespace AiPrompter.Runtime.Services;
 
-public class NewsDataPollerService(AppSettings appSettings) : INewsDataPollerService
+public class NewsDataPollerService(AppSettings appSettings, DataContext context) : INewsDataPollerService
 {
     public async Task<Dictionary<Industries, List<MarketauxGetNewsByCategoryResponse>>> GetCategoryNewsAsync()
     {
@@ -28,6 +31,55 @@ public class NewsDataPollerService(AppSettings appSettings) : INewsDataPollerSer
         }
 
         return categoryResponses;
+    }
+
+    public async Task<Dictionary<Stock, List<MarketauxGetNewsByCategoryResponse>>> GetStockNewsAsync()
+    {
+        var response = new Dictionary<Stock, List<MarketauxGetNewsByCategoryResponse>>();
+
+        var stocks = await context.Stocks.ToListAsync();
+
+        foreach (var stock in stocks) 
+        {
+            var newsPositive = await GetSingleStockNews(stock.Symbol, "desc");
+            var newsNegative = await GetSingleStockNews(stock.Symbol, "asc");
+
+            response[stock] = [newsPositive, newsNegative];
+        }
+
+        return response;
+    }
+
+    private async Task<MarketauxGetNewsByCategoryResponse> GetSingleStockNews(string symbol, string order)
+    {
+        var client = new RestClient(appSettings.MarketauxApiBaseUrl);
+
+        var request = new RestRequest();
+        request.AddQueryParameter("api_token", appSettings.MarketauxApiKeyStock);
+        request.AddQueryParameter("symbols", symbol);
+        request.AddQueryParameter("filter_entities", "true");
+        request.AddQueryParameter("published_after", DateTime.Now.AddDays(-1).ToString("yyyy-MM-ddTHH:mm"));
+        request.AddQueryParameter("sort", "entity_sentiment_score");
+        request.AddQueryParameter("sort_order", order);
+        request.AddQueryParameter("language", "en");
+
+        var response = await client.ExecuteAsync(request);
+
+        if (response.IsSuccessStatusCode is false)
+        {
+            throw new HttpRequestException("Getting news by Category failed!");
+        }
+
+        if (string.IsNullOrEmpty(response.Content))
+        {
+            throw new HttpRequestException("Response is null or empty");
+        }
+
+        var stringResult = response.Content;
+
+        MarketauxGetNewsByCategoryResponse content = JsonSerializer.Deserialize<MarketauxGetNewsByCategoryResponse>(stringResult);
+
+        return content;
     }
 
     private async Task<MarketauxGetNewsByCategoryResponse> GetSingleCategoryNews(Industries industry, string order)
