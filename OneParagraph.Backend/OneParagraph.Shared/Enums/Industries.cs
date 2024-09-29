f9057a9d1fd9a9b3c7e2597f076906fdcdd9d2a8
Adam Ciszewski <79523918+OlivierMiracle@users.noreply.github.com>
2024-09-29 06:59:31 +0200

    develop to main great merge (#24)
    * Feature/login (#5)
    
    * Add Shared project
    
    * Add aspnet identity
    
    * Adds swagger and identity migration
    
    * Adds CategorynewsDataPoller (#6)
    
    * Stuff
    
    * Adds getting news by category
    
    * Adds industry paragraph endpoint (#7)
    
    * Finished news poller (#8)
    
    * Adds missing semicolon (#10)
    
    * Feature/stock endpoint (#9)
    
    * Adds industry paragraph endpoint
    
    * Working swagger configuration
    
    * Adds endpoints detection
    
    * Create azure-web-app-dotnet.yml
    
    bdfkljghnosdri
    
    * Update azure-web-app-dotnet.yml
    
    again
    
    * Update azure-web-app-dotnet.yml
    
    będą to widzieć
    
    * Feature/stocks (#16)
    
    * Adds migration with stocks
    
    * Adds two endpoints for stocks
    
    * Adds Azure AI Services and many more. (#17)
    
    * fixes everything (#18)
    
    * Improves industryParagraphs endpoint (#19)
    
    * Feature/stock pooler (#20)
    
    * Adds stock request
    
    * Some stuff
    
    * Update README.md
    
    * Prints Industry enum to string value (#23)
    
    ---------
    
    Co-authored-by: m653l <83173100+m653l@users.noreply.github.com>

--- OneParagraph.Backend/OneParagraph.Shared/Enums/Industries.cs
+++ OneParagraph.Backend/OneParagraph.Shared/Enums/Industries.cs
@@ -1,7 +1,10 @@
 using System.ComponentModel;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using OneParagraph.Shared.Extensions;
 
 namespace OneParagraph.Shared.Enums;
-
+[JsonConverter(typeof(JsonDescriptionToEnumConverter))]
 public enum Industries
 {
     [Description("Basic Materials")]
@@ -51,4 +54,24 @@ public enum Industries
 
     [Description("N/A")]
     NA
+}
+
+public class JsonDescriptionToEnumConverter : JsonConverter<Industries>
+{
+    public override Industries Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        List<Industries> industriesList = Enum.GetValues<Industries>().ToList();
+
+        foreach (var industry in industriesList)
+        {
+            if (industry.GetEnumDescription() == reader.GetString()) return industry;
+        }
+
+        return Industries.BasicMaterials;
+    }
+
+    public override void Write(Utf8JsonWriter writer, Industries value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.GetEnumDescription());
+    }
 }
